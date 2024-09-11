fd566bfaf99d4d56f39c081b02471a247e9d76b7
michalrusak <michal1406@onet.pl>
2024-09-11 10:04:10 +0200

    add sorting to table

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import { IUser } from "../interfaces/user.interface";
 
 const TableComponent: React.FC = () => {
   const users = useSelector((state: RootState) => state.users.filteredUsers);
+
+  const [sortConfig, setSortConfig] = useState<{
+    key: keyof IUser;
+    direction: "asc" | "desc";
+  } | null>(null);
+
+  const sortedUsers = React.useMemo(() => {
+    const sortableUsers = [...users];
+    if (sortConfig !== null) {
+      sortableUsers.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "asc" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "asc" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableUsers;
+  }, [users, sortConfig]);
+
+  const handleSort = (key: keyof IUser) => {
+    let direction: "asc" | "desc" = "asc";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "asc"
+    ) {
+      direction = "desc";
+    }
+    setSortConfig({ key, direction });
+  };
 
   return (
     <div
@@ -16,14 +50,54 @@ const TableComponent: React.FC = () => {
       <table className="table table-striped table-hover mx-2 my-3 p-2">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Phone</th>
+            <th
+              onClick={() => handleSort("name")}
+              style={{ cursor: "pointer" }}
+            >
+              Name{" "}
+              {sortConfig?.key === "name"
+                ? sortConfig.direction === "asc"
+                  ? "↑"
+                  : "↓"
+                : null}
+            </th>
+            <th
+              onClick={() => handleSort("username")}
+              style={{ cursor: "pointer" }}
+            >
+              Username{" "}
+              {sortConfig?.key === "username"
+                ? sortConfig.direction === "asc"
+                  ? "↑"
+                  : "↓"
+                : null}
+            </th>
+            <th
+              onClick={() => handleSort("email")}
+              style={{ cursor: "pointer" }}
+            >
+              Email{" "}
+              {sortConfig?.key === "email"
+                ? sortConfig.direction === "asc"
+                  ? "↑"
+                  : "↓"
+                : null}
+            </th>
+            <th
+              onClick={() => handleSort("phone")}
+              style={{ cursor: "pointer" }}
+            >
+              Phone{" "}
+              {sortConfig?.key === "phone"
+                ? sortConfig.direction === "asc"
+                  ? "↑"
+                  : "↓"
+                : null}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
+          {sortedUsers.map((user) => (
             <tr key={user.id}>
               <td>{user.name}</td>
               <td>{user.username}</td>
